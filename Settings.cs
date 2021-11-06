be1392e95555dd640f47faa9c10e4d45c45e2730
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-11-05 19:47:20 -0700

    Increase default knuckle damper rate

--- Settings.cs
+++ Settings.cs
@@ -17,7 +17,7 @@ namespace DvMod.ZCouplers
 
         [Draw("Knuckle strength (Mn)", Min = 0.1f, VisibleOn = "couplerType|JanneyKnuckle")] public float knuckleStrength = 1.85f;
         [Draw("Compression spring rate", Min = 0f, VisibleOn = "couplerType|JanneyKnuckle")] public float drawgearSpringRate = 4f;
-        [Draw("Compression damper rate", Min = 0f, VisibleOn = "couplerType|JanneyKnuckle")] public float drawgearDamperRate = 8f;
+        [Draw("Compression damper rate", Min = 0f, VisibleOn = "couplerType|JanneyKnuckle")] public float drawgearDamperRate = 16f;
         [Draw("Auto couple threshold (mm)", Min = 0f, VisibleOn = "couplerType|JanneyKnuckle")] public float autoCoupleThreshold = 20f;
 
         [Draw("Enable logging")] public bool enableLogging = false;
