0f8b36e23813d9f265ddb40837db300a5d172267
mspielberg <9729801+mspielberg@users.noreply.github.com>
2022-07-04 17:22:37 -0700

    Persist coupler lock state in save

---
+++ SaveManager.cs
@@ -0,0 +1,46 @@
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+
+namespace DvMod.ZCouplers
+{
+    public static class SaveManager
+    {
+        private const string SaveKey = "DvMod.ZCouplers";
+        private const string FrontCouplerLockedKey = "frontCouplerLocked";
+        private const string RearCouplerLockedKey = "rearCouplerLocked";
+
+        [HarmonyPatch(typeof(CarsSaveManager), nameof(CarsSaveManager.GetCarSaveData))]
+        public static class GetCarSaveDataPatch
+        {
+            public static void Postfix(TrainCar car, JObject __result)
+            {
+                __result[SaveKey] = new JObject(
+                    new JProperty(FrontCouplerLockedKey, KnuckleCouplers.IsReadyToCouple(car.frontCoupler)),
+                    new JProperty(RearCouplerLockedKey, KnuckleCouplers.IsReadyToCouple(car.rearCoupler)));
+            }
+        }
+
+        [HarmonyPatch(typeof(CarsSaveManager), nameof(CarsSaveManager.InstantiateCar))]
+        public static class InstantiateCarPatch
+        {
+            public static void Postfix(JObject carData, TrainCar __result)
+            {
+                static void SetupCoupler(Coupler coupler, bool locked)
+                {
+                    if (locked)
+                        KnuckleCouplers.ReadyCoupler(coupler);
+                    else
+                        KnuckleCouplers.UnlockCoupler(coupler, true);
+                }
+
+                if ((carData?.TryGetValue(SaveKey, out var data) ?? false) && data is JObject obj)
+                {
+                    if (obj.TryGetValue(FrontCouplerLockedKey, out var frontLocked))
+                        SetupCoupler(__result.frontCoupler, frontLocked.Value<bool>());
+                    if (obj.TryGetValue(RearCouplerLockedKey, out var rearLocked))
+                        SetupCoupler(__result.rearCoupler, rearLocked.Value<bool>());
+                }
+            }
+        }
+    }
+}
