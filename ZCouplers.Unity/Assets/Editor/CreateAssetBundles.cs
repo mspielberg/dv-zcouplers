58951d0f3886a5ff902ce8a70afc43dd137fd1e1
Fuggschen <fuggschen@krk-gaming.de>
2025-08-18 02:18:02 +0200

    feat: Added Unity project to sourcecode
    Signed-off-by: Fuggschen <fuggschen@krk-gaming.de>

---
+++ ZCouplers.Unity/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles()
+    {
+        string bundleDir = "Assets/AssetBundles";
+        if (!Directory.Exists(bundleDir))
+        {
+            Directory.CreateDirectory(bundleDir);
+        }
+
+        BuildPipeline.BuildAssetBundles(bundleDir, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+    }
+}
